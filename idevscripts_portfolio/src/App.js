2702c7ba906d36ccc08fe1a7899d9b3f2bfeac6c
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-20 16:02:28 -0400

    Created GlassSection

--- idevscripts_portfolio/src/App.js
+++ idevscripts_portfolio/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import AboutMe from './components/AboutMe';
+import './styles/app.scss';
 function App() {
   return (
     <div className="App">
-      
+      <AboutMe/>
     </div>
   );
 }
---
+++ idevscripts_portfolio/src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => {
+
+    return(
+        <div className="main">
+             <div className="glass"></div>
+        </div>
+    )
+}
+
+
+export default AboutMe;
