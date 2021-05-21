830662a88dcc9600a399b7eef474078ec9cb5e93
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-20 23:24:42 -0400

    Updated UserSection

--- idevscripts_portfolio/src/components/AboutMe.js
+++ idevscripts_portfolio/src/components/AboutMe.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import profile from '../img/profile.png';
 
 const AboutMe = () => {
 
     return(
         <div className="main">
-             <div className="glass"></div>
+             <div className="glass">
+                <div className="aboutSection">
+                    <div className="user">
+                        <img src={profile} alt=""/>
+                        <h3>Isaiah DeSantis</h3>
+                        <p>Software Engineer</p>
+                    </div>
+                    <div className="skills">
+                        <div className="languages">
+                            <h3>Langauges/Frameworks</h3>
+                            <p>Javascript</p>
+                            <p>Java</p>
+                            <p>Reactjs</p>
+                            <p>React Native</p>
+                            <p>MongoDB</p>
+                        </div>
+                    </div>
+                    <div className="education">
+                        <h3>Education</h3>
+                        <p>BS in Computer Science- Cheyney University</p>
+                    </div>
+                </div>
+                <div className="projects">
+                    <h3>Projects</h3>
+                </div>
+             </div>
         </div>
     )
 }
