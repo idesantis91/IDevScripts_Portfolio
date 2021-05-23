56fbaf755fe1daabd8d4e22bcb0e2f8231171ad4
Isaiah DeSantis <42743832+idesantis91@users.noreply.github.com>
2021-05-23 14:03:42 -0400

    Added Icons

--- idevscripts_portfolio/src/components/AboutMe.js
+++ idevscripts_portfolio/src/components/AboutMe.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import profile from '../img/profile.png';
+//Animations
+import { motion } from "framer-motion";
+//Icons
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { ImMail } from "react-icons/im";
+
 
 const AboutMe = () => {
 
+
+
     return(
         <div className="main">
+            <div className="devName">
+                <h3>IDevScripts</h3>
+            </div>
              <div className="glass">
                 <div className="aboutSection">
                     <div className="user">
@@ -28,7 +40,71 @@ const AboutMe = () => {
                     </div>
                 </div>
                 <div className="projects">
-                    <h3>Projects</h3>
+                    <h2>Projects</h2>
+                    <div className="project-list">
+                    <motion.div
+                        className='container'
+                        whileHover={{ scale: 1.7, rotate: 360 }}
+                    >
+                    <h3>ShapeShot</h3>
+                    <p>
+                        A 1v1 Shape Shooter, where the last Shape Standing Wins!
+                    </p>
+                    </motion.div>
+                    <motion.div
+                        className='container'
+                        whileHover={{ scale: 1.7, rotate: 360 }}
+                    >
+                    <h3>StudyQuiz</h3>
+                    <p>
+                        A React Application that allows you to create 
+                    a study guide to study for exams or just to have fun!
+                    </p>
+                    </motion.div>
+                    <motion.div
+                        className='container'
+                        whileHover={{ scale: 1.7, rotate: 360 }} 
+                    >
+                    <h3>First Assembly App</h3>
+                    <p>Created an App for First Assembly of God (React Native)</p>
+                    </motion.div>
+                    </div>
+
+                    <div className="contact">
+                    <h2>Contact Me</h2>
+                    <div className="contact-list">
+                    <motion.div
+                        className='container-contact'
+                        whileHover={{ scale: 1.7, rotate: 360 }}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            window.location.href='https://github.com/idesantis91';
+                         }} 
+                    >
+                    <FaGithub size='4rem' color='gray' className='icon'/>
+                    </motion.div>
+                    <motion.div
+                        className='container-contact'
+                        whileHover={{ scale: 1.7, rotate: 360 }} 
+                        onClick={(e) => {
+                            e.preventDefault();
+                            window.location.href=('mailto:IDevScript@gmail.com')
+                         }}
+                    >
+                    <ImMail size='4rem' color='gray' className='icon'/>
+                    </motion.div>
+                    <motion.div
+                        className='container-contact'
+                        whileHover={{ scale: 1.7, rotate: 360 }}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            window.location.href='https://www.linkedin.com/in/isaiahdesantis/';
+                         }}
+                    >
+                    <FaLinkedin size='4rem' color='gray' className='icon'/>
+                    </motion.div>
+                    </div>
+                    </div>
                 </div>
              </div>
         </div>
